f366ff42de305fa34103c0c194bbd014403aace9
ar00061122 <ar00061122@gmail.com>
2025-08-07 11:27:17 +0000

    fixed api key

--- api/language/[language]/proxy.js
+++ api/language/[language]/proxy.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
         {
           method: "GET",
           headers: {
-            "x-api-key": import.meta.env.VITE_API_GATEWAY_KEY, // secure
+            "x-api-key": process.env.VITE_API_GATEWAY_KEY, // secure
           },
         }
       );
